26860a1175e7741b33239a7349caccc9c8d96b4a
sujoybag <sujoy.bag2011@gmail.com>
2021-01-21 13:02:24 +0530

    Update app.py

--- app.py
+++ app.py
@@ -26,9 +26,9 @@ options.add_argument("headless") # Runs Chrome in headless mode.
 # options.add_argument('--no-sandbox') # Bypass OS security model
 options.add_argument('--disable-gpu')  # applicable to windows os only
 
-ABDCpath="template/File/ABDC_2019.xlsx"
-SCOPUSpath="template/File/SCOPUS_2018.xlsx"
-chromepath="template/File/chromedriver.exe"
+ABDCpath="https://github.com/sujoybag/gs-profile/blob/main/template/File/ABDC_2019.xlsx"
+SCOPUSpath="https://github.com/sujoybag/gs-profile/blob/main/template/File/SCOPUS_2018.xlsx"
+chromepath="https://github.com/sujoybag/gs-profile/blob/main/template/File/chromedriver.exe"
 
 app = Flask(__name__, template_folder='template')
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
