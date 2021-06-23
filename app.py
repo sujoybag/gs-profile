60bb9a6a0a975bd01e348e5689e838851a295b3c
sujoybag <sujoy.bag2011@gmail.com>
2021-06-23 23:05:39 +0530

    Update app.py

--- app.py
+++ app.py
@@ -127,14 +127,14 @@ def getfile():
             dfd['Publication_Name']=df_Publication_Name['Publication_Name']
             dfd['# of Articles']=df_Publication_Name['# of Articles']
             dfd['Avg Citations']=df_citations['Avg Citations']
-            ABDC=pd.read_excel(r"https://raw.githubusercontent.com/sujoybag/gs-profile/ABDC_2019.xlsx",sep=",")
+            ABDC=pd.read_excel(r"https://raw.githubusercontent.com/sujoybag/gs-profile/ABDC_2019.xlsx")
             ABDC['Publication_Name']=ABDC['Publication_Name'].str.replace("&", "And")
             ABDC['Publication_Name']=ABDC['Publication_Name'].str.replace(",","")
             ABDC['Publication_Name']=ABDC['Publication_Name'].str.title()
             ABDC=ABDC[['Publication_Name','Year of Inception','Publication Rank (ABDC)']]
             final_df=pd.DataFrame()
             temp_df=pd.merge(dfd,ABDC,on='Publication_Name',how='left')
-            SCOPUS=pd.read_excel(r"https://raw.githubusercontent.com/sujoybag/gs-profile/SCOPUS_2018.xlsx",sep=",")
+            SCOPUS=pd.read_excel(r"https://raw.githubusercontent.com/sujoybag/gs-profile/SCOPUS_2018.xlsx")
             SCOPUS['Publication_Name']=SCOPUS['Publication_Name'].str.replace("&", "And")
             SCOPUS['Publication_Name']=SCOPUS['Publication_Name'].str.replace(",","")
             SCOPUS['Publication_Name']=SCOPUS['Publication_Name'].str.title()
