45d345498534022080cb7ddcb947759eec8a09b4
sujoybag <sujoy.bag2011@gmail.com>
2021-07-13 21:02:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -22,7 +22,7 @@ from bs4 import BeautifulSoup
 
 chrome_options = Options()
 chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
-chrome_options.add_argument("--headless")
+# chrome_options.add_argument("--headless")
 chrome_options.add_argument("--disable-dev-shm-usage")
 chrome_options.add_argument("--no-sandbox")
 
